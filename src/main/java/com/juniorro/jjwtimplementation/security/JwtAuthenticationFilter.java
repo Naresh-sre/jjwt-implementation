3212f0b44153eba06e65f9c37980c36438e28576
juniorro <juniorro98@outlook.com>
2018-07-10 23:52:53 -0400

    Angular frontend app added

--- src/main/java/com/juniorro/jjwtimplementation/security/JwtAuthenticationFilter.java
+++ src/main/java/com/juniorro/jjwtimplementation/security/JwtAuthenticationFilter.java
@@ -26,7 +26,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	private AuthenticationManager authenticationManager;
 	
 	public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
-		super();
 		this.authenticationManager = authenticationManager;
 	}
 
