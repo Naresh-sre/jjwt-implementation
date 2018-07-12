ec1f242d5a5fb1ee0c033b3cb60f0aeed1f5fb8a
juniorro <juniorro98@outlook.com>
2018-07-12 01:07:10 -0400

    Allow CORS in Authorization filter class

--- src/main/java/com/juniorro/jjwtimplementation/security/JwtAuthorizationFilter.java
+++ src/main/java/com/juniorro/jjwtimplementation/security/JwtAuthorizationFilter.java
@@ -24,25 +24,44 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain finterChain)
 			throws ServletException, IOException {
-		String jwtToken = request.getHeader(SecurityConstants.HEADER_TYPE);
-		if(jwtToken == null || !jwtToken.startsWith(SecurityConstants.TOKEN_PREFIX)) {
-			finterChain.doFilter(request, response);
-			return;
-		}		
-		Claims claims = Jwts.parser()
-				.setSigningKey(SecurityConstants.SECRET)
-				.parseClaimsJws(jwtToken.replace(SecurityConstants.TOKEN_PREFIX, ""))
-				.getBody();
-		String username = claims.getSubject();
-		ArrayList<Map<String, String>> roles = (ArrayList<Map<String, String>>) claims.get("roles");
-		Collection<GrantedAuthority> authorities = new ArrayList<>();
-		roles.forEach(role->
-			authorities.add(new SimpleGrantedAuthority(role.get("authority"))));
-		UsernamePasswordAuthenticationToken authenticatedUser = new UsernamePasswordAuthenticationToken(
-				username, null, authorities);
-		SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
-		finterChain.doFilter(request, response);
 		
+		response.addHeader("Access-Control-Allow-Origin", SecurityConstants.CLIENT_DOMAIN_URL);
+		
+		response.addHeader("Access-Control-Allow-Headers", 
+				"Origin, Accept, X-Requested-With, "
+				+ "Content-Type, Access-Control-Request-Method, "
+				+ "Access-Control-Request-Headers, Authorization");
+		
+		response.addHeader("Access-Control-Expose-Headers", 
+				"Access-Control-Allow-Origin, "
+				+ "Access-Control-Allow-Creentials, "
+				+ "Authorization");
+		
+		if ((request.getMethod().equalsIgnoreCase("OPTIONS"))) {
+			try {
+				response.setStatus(HttpServletResponse.SC_OK);
+			} catch (Exception e) {
+				e.printStackTrace();
+				}
+			} else {
+				String jwtToken = request.getHeader(SecurityConstants.HEADER_TYPE);
+				if(jwtToken == null || !jwtToken.startsWith(SecurityConstants.TOKEN_PREFIX)) {
+					finterChain.doFilter(request, response);
+					return;
+				}	
+				Claims claims = Jwts.parser()
+						.setSigningKey(SecurityConstants.SECRET)
+						.parseClaimsJws(jwtToken.replace(SecurityConstants.TOKEN_PREFIX, ""))
+						.getBody();
+				String username = claims.getSubject();
+				ArrayList<Map<String, String>> roles = (ArrayList<Map<String, String>>) claims.get("roles");
+				Collection<GrantedAuthority> authorities = new ArrayList<>();
+				roles.forEach(role->
+					authorities.add(new SimpleGrantedAuthority(role.get("authority"))));
+				UsernamePasswordAuthenticationToken authenticatedUser = new UsernamePasswordAuthenticationToken(
+						username, null, authorities);
+				SecurityContextHolder.getContext().setAuthentication(authenticatedUser);
+				finterChain.doFilter(request, response);
+			}		
 	}
-
 }
--- src/main/java/com/juniorro/jjwtimplementation/security/SecurityConstants.java
+++ src/main/java/com/juniorro/jjwtimplementation/security/SecurityConstants.java
@@ -6,4 +6,6 @@ public class SecurityConstants {
 	public static final long EXPIRATION_TIME = 432_000_000; // 5 days
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_TYPE = "Authorization";
+//	public static final String CLIENT_DOMAIN_URL = "http://localhost:4200/**";
+	public static final String CLIENT_DOMAIN_URL = "*";
 }
